d042b1fba7a1399288fa198d55284bafc44cfd0a
Roberto Alvarez <robertoatc02@gmail.com>
2021-06-02 18:53:23 -0500

    fix: some errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,15 +7,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img alt="logo" className="App-logo" src={logo} />
+        <p>trash</p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
+import { SetupWorkerApi } from 'msw';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line
+  const { worker } = require('./mocks/browser') as { worker: SetupWorkerApi };
+
+  void worker.start();
+}
+
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
